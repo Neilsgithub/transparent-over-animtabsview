fce7bd23f3756464646a38fbc81f9703471b61ba
Wuyexiong <wuyexiong@xiaomi.com>
2014-05-21 00:02:27 +0800

    滑动跟随

--- animtabsview.demo/src/tree/love/animtabsview/demo/MainActivity.java
+++ animtabsview.demo/src/tree/love/animtabsview/demo/MainActivity.java
@@ -1,9 +1,13 @@
+
 package tree.love.animtabsview.demo;
 
 import tree.love.animtabsview.AnimTabsView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,48 +17,69 @@ public class MainActivity extends FragmentActivity {
 
     private static final String TAG = "MainActivity";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		
-		private AnimTabsView mTabsView;
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
 
-		public PlaceholderFragment() {
-		}
+        private AnimTabsView mTabsView;
+        private ViewPager mViewPager;
+        private MyPagerAdapter mPagerAdapter;
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			setupViews(rootView);
-			return rootView;
-		}
-		
-		private void setupViews(View rootView) {
-			mTabsView = (AnimTabsView) rootView.findViewById(R.id.publiclisten_tab);
-			mTabsView.addItem("推荐");
-			mTabsView.addItem("排行榜");
-			mTabsView.addItem("歌单");
-			mTabsView.addItem("DJ节目");
+        public PlaceholderFragment() {
+        }
 
-            mTabsView.setOnAnimTabsItemViewChangeListener(new AnimTabsView.IAnimTabsItemViewChangeListener() {
-                @Override
-                public void onChange(AnimTabsView tabsView, int oldPosition, int currentPosition) {
-                }
-            });
-		}
-	}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            setupViews(rootView);
+            return rootView;
+        }
+
+        private void setupViews(View rootView) {
+            mTabsView = (AnimTabsView) rootView.findViewById(R.id.publiclisten_tab);
+            mPagerAdapter = new MyPagerAdapter(getChildFragmentManager());
+            mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
+            mViewPager.setAdapter(mPagerAdapter);
+            mTabsView.setViewPager(mViewPager);
+        }
+
+        public class MyPagerAdapter extends FragmentPagerAdapter {
+
+            private final String[] TITLES = { "推荐", "排行榜", "歌单", "DJ节目"};
+
+            public MyPagerAdapter(FragmentManager fm) {
+                super(fm);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return TITLES[position];
+            }
+
+            @Override
+            public int getCount() {
+                return TITLES.length;
+            }
+
+            @Override
+            public Fragment getItem(int position) {
+                return SuperAwesomeCardFragment.newInstance(position);
+            }
+
+        }
+    }
 
 }
--- animtabsview/src/tree/love/animtabsview/AnimTabsView.java
+++ animtabsview/src/tree/love/animtabsview/AnimTabsView.java
@@ -1,180 +1,286 @@
-package tree.love.animtabsview;
 
-import java.lang.ref.WeakReference;
+package tree.love.animtabsview;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.Scroller;
 import android.widget.TextView;
 
 public class AnimTabsView extends RelativeLayout {
-	
-	private static final int ANIM_DURATION = 600;
-	
-	private Context mContext;
-	private int mTotalItemsCount = 0;
-	private int mCurrentItemPosition = 0;
-	
-	private LinearLayout mItemsLayout;
-	private Scroller mScroller;
-	private Bitmap mSlideIcon;
-	private Bitmap mShadow;
-	private Rect mLeftDrawRect;
-	private Rect mRightDrawRect;
-	
-	private int mCurrentSlideX;
-	private int mCurrentSlideY;
-	
-	
-	private IAnimTabsItemViewChangeListener mItemViewChangeListener;
-	private View.OnClickListener mItemClickListener = new TabsItemViewClickListener(this);
 
-	public AnimTabsView(Context context) {
-		this(context, null);
-	}
+//    private static final String TAG = "AnimTabsView";
 
-	public AnimTabsView(Context context, AttributeSet attributeset) {
-		super(context, attributeset);
-		
-		setWillNotDraw(false);
-		
-		this.mContext = context;
-		this.mLeftDrawRect = new Rect();
-		this.mRightDrawRect = new Rect();
-		this.mSlideIcon = BitmapFactory.decodeResource(getResources(), R.drawable.blk_menubtn_arr);
-		this.mShadow = BitmapFactory.decodeResource(getResources(), R.drawable.blk_menubtn_shadow);
-		LinearLayout slideLayout = new LinearLayout(this.mContext);
-		slideLayout.setOrientation(LinearLayout.VERTICAL);
-		View localView = new View(this.mContext);
-		localView.setBackgroundResource(R.drawable.blk_menubtn_bg);
-		slideLayout.addView(localView, new LinearLayout.LayoutParams(-1, 0, 1.0F));
-		slideLayout.addView(new View(this.mContext), new LinearLayout.LayoutParams(-1, this.mSlideIcon.getHeight()));
-		this.mItemsLayout = new LinearLayout(this.mContext);
-		this.mItemsLayout.setBackgroundColor(0);
-		RelativeLayout.LayoutParams localLayoutParams = new RelativeLayout.LayoutParams( -1, -1);
-		float f1 = context.getResources().getDisplayMetrics().density;
-//		this.mItemsLayout.setPadding((int) (f1 * 20), 0, (int) (f1 * 20), 0);
-		localLayoutParams.setMargins(0, 0, 0, (int) (f1 * 4.0F));
-		addView(slideLayout);
-		addView(this.mItemsLayout, localLayoutParams);
-	}
+    private Context mContext;
+    private int mTotalItemsCount = 0;
+    private int mCurrentItemPosition = 0;
 
-	public int getCount() {
-		return this.mTotalItemsCount;
-	}
+    private LinearLayout mItemsContainerLayout;
+    private Bitmap mSlideIcon;
+    private Bitmap mShadow;
+    private Rect mLeftDrawRect;
+    private Rect mRightDrawRect;
+    private int mCurrentSlideX;
+    private int mCurrentSlideY;
 
-	public void selecteItem(int selectePosition) {
-		selecteItem(selectePosition, true);
-	}
+    private final PageListener mPageListener = new PageListener();
+    private ViewPager.OnPageChangeListener mDelegatePageListener;
+    private ViewPager mViewPager;
 
-	public void selecteItem(int selectePosition, boolean isAnimate) {
-		if (this.mCurrentItemPosition == selectePosition)
-			return;
-		if (this.mScroller == null) {
-			this.mScroller = new Scroller(this.mContext, new DecelerateInterpolator());
-		}
-		((RelativeLayout) getItemView(this.mCurrentItemPosition)).setSelected(false);
-		this.mCurrentItemPosition = selectePosition;
-		getItemView(this.mCurrentItemPosition).setSelected(true);
-		int oldX = this.mCurrentSlideX;
-		int oldY = this.mCurrentSlideY;
-		if ((this.mTotalItemsCount > 0) && (getItemView(this.mCurrentItemPosition) != null)) {
-			View currentSeletedItemView = getItemView(this.mCurrentItemPosition);
-			this.mCurrentSlideX = (currentSeletedItemView.getLeft() + currentSeletedItemView.getWidth() / 2 - this.mSlideIcon .getWidth() / 2);
-			if (isAnimate) {				
-				this.mScroller.startScroll(oldX, oldY, this.mCurrentSlideX - oldX, this.mCurrentSlideY - oldY, ANIM_DURATION);
-			}
-		}
-		invalidate();
-	}
+    private int mScrollState;
+    private boolean isFirstLayout;
 
-	public void setOnAnimTabsItemViewChangeListener(IAnimTabsItemViewChangeListener listener) {
-		this.mItemViewChangeListener = listener;
-	}
 
-	public void addItem(String itemText) {
-		RelativeLayout itemLayout = (RelativeLayout) View.inflate(this.mContext, R.layout.anim_tab_item, null);
-		((TextView) itemLayout.getChildAt(0)).setText(itemText);
-		itemLayout.setTag(Integer.valueOf(this.mTotalItemsCount));
-		if (this.mTotalItemsCount == 0) {			
-			itemLayout.setSelected(true);
-		}
-		itemLayout.setOnClickListener(this.mItemClickListener);
-		this.mTotalItemsCount = (1 + this.mTotalItemsCount);
-		this.mItemsLayout.addView(itemLayout, new LinearLayout.LayoutParams(0, -1, 1.0F));
-	}
+    public AnimTabsView(Context context) {
+        this(context, null);
+    }
 
-	public int getCurrentItemPosition() {
-		return this.mCurrentItemPosition;
-	}
+    public AnimTabsView(Context context, AttributeSet attributeset) {
+        super(context, attributeset);
+        setWillNotDraw(false);
 
-	public View getItemView(int itemPosition) {
-		if ((itemPosition >= 0) && (itemPosition < this.mTotalItemsCount)) {
-			return this.mItemsLayout.getChildAt(itemPosition);
-		}
-		return null;
-	}
+        this.mContext = context;
+        this.mLeftDrawRect = new Rect();
+        this.mRightDrawRect = new Rect();
+        this.mSlideIcon = BitmapFactory.decodeResource(getResources(), R.drawable.blk_menubtn_arr);
+        this.mShadow = BitmapFactory.decodeResource(getResources(), R.drawable.blk_menubtn_shadow);
+        LinearLayout slideLayout = new LinearLayout(this.mContext);
+        slideLayout.setOrientation(LinearLayout.VERTICAL);
+        View localView = new View(this.mContext);
+        localView.setBackgroundResource(R.drawable.blk_menubtn_bg);
+        slideLayout.addView(localView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 1.0F));
+        slideLayout.addView(new View(this.mContext), new LinearLayout.LayoutParams(-1, this.mSlideIcon.getHeight()));
+        this.mItemsContainerLayout = new LinearLayout(this.mContext);
+        this.mItemsContainerLayout.setBackgroundColor(0);
+        RelativeLayout.LayoutParams localLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        float f1 = context.getResources().getDisplayMetrics().density;
+        localLayoutParams.setMargins(0, 0, 0, (int) (f1 * 4.0F));
+        addView(slideLayout);
+        addView(this.mItemsContainerLayout, localLayoutParams);
+    }
 
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		if ((this.mScroller == null) || (!this.mScroller.computeScrollOffset())) {
-			this.mLeftDrawRect.set(0, this.mCurrentSlideY, this.mCurrentSlideX, this.mCurrentSlideY + this.mShadow.getHeight());
-			canvas.drawBitmap(this.mShadow, null, this.mLeftDrawRect, null);
-			this.mRightDrawRect.set(this.mCurrentSlideX + this.mSlideIcon.getWidth(), this.mCurrentSlideY, getWidth(), this.mCurrentSlideY
-					+ this.mShadow.getHeight());
-			canvas.drawBitmap(this.mShadow, null, this.mRightDrawRect, null);
-			canvas.drawBitmap(this.mSlideIcon, this.mCurrentSlideX, this.mCurrentSlideY, null);
-			return;
-		}
-		int scrollX = this.mScroller.getCurrX();
-		int scrollY = this.mScroller.getCurrY();
-		this.mLeftDrawRect.set(0, scrollY, scrollX, scrollY + this.mShadow.getHeight());
-		canvas.drawBitmap(this.mShadow, null, this.mLeftDrawRect, null);
-		this.mRightDrawRect.set(scrollX + this.mSlideIcon.getWidth(), scrollY, getWidth(),
-				scrollY + this.mShadow.getHeight());
-		canvas.drawBitmap(this.mShadow, null, this.mRightDrawRect, null);
-		canvas.drawBitmap(this.mSlideIcon, scrollX, scrollY, null);
-		invalidate();
-	}
+    public int getCount() {
+        return this.mTotalItemsCount;
+    }
 
-	@Override
-	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		super.onLayout(changed, l, t, r, b);
-		if ((this.mTotalItemsCount > 0) && (getItemView(this.mCurrentItemPosition) != null)) {
-			View currentItemView = getItemView(this.mCurrentItemPosition);
-			this.mCurrentSlideX = (currentItemView.getLeft() + currentItemView.getWidth() / 2 - this.mSlideIcon.getWidth() / 2);
-			this.mCurrentSlideY = (b - t - this.mSlideIcon.getHeight());
-		}
-	}
+    private void scrollToTab(int tabIndex, int positionOffset) {
+        if ((this.mTotalItemsCount > 0) && (getItemView(tabIndex) != null)) {
+            View currentSeletedItemView = getItemView(tabIndex);
+            this.mCurrentSlideX = (currentSeletedItemView.getLeft()
+                    + currentSeletedItemView.getWidth() / 2 - this.mSlideIcon.getWidth() / 2)
+                    + positionOffset;
+            invalidate();
+//            Log.d(TAG, "scrollToTab >> mCurrentSlideX" + mCurrentSlideX);
+        }
+    }
 
-	private static class TabsItemViewClickListener implements View.OnClickListener {
-		
-		private WeakReference<AnimTabsView> mAnimTabsViewReference = null;
-		
-		TabsItemViewClickListener(AnimTabsView animTabsView) {
-			mAnimTabsViewReference = new WeakReference<AnimTabsView>(animTabsView);
-		}
+    public void addItem(final int position, String itemText) {
+        RelativeLayout itemLayout = (RelativeLayout) View.inflate(this.mContext, R.layout.anim_tab_item, null);
+        ((TextView) itemLayout.getChildAt(0)).setText(itemText);
+        itemLayout.setTag(Integer.valueOf(position));
+        itemLayout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(position);
+            }
+        });
+        this.mItemsContainerLayout.addView(itemLayout, new LinearLayout.LayoutParams(0, -1, 1.0F));
+    }
 
-		public void onClick(View view) {
-			int clickItemPosition = ((Integer) view.getTag()).intValue();
-			int currentSeletedItemPosition = (mAnimTabsViewReference.get().getCurrentItemPosition());
-			if (currentSeletedItemPosition != clickItemPosition && mAnimTabsViewReference.get() != null) {
-				mAnimTabsViewReference.get().selecteItem(clickItemPosition);
-				if (mAnimTabsViewReference.get().mItemViewChangeListener != null)
-					mAnimTabsViewReference.get().mItemViewChangeListener.onChange(mAnimTabsViewReference.get(), clickItemPosition, currentSeletedItemPosition);
-			}
-		}
-	}
+    private void setCurrentItemSelected(int position) {
+        for (int i = 0; i < mTotalItemsCount; i++) {
+            if (i == position) {
+                getItemView(i).setSelected(true);
+            } else {
+                getItemView(i).setSelected(false);
+            }
+        }
+    }
 
-	public abstract interface IAnimTabsItemViewChangeListener {
-		public abstract void onChange(AnimTabsView tabsView, int oldPosition, int currentPosition);
-	}
+    public int getCurrentItemPosition() {
+        return this.mCurrentItemPosition;
+    }
+
+    public View getItemView(int itemPosition) {
+        if ((itemPosition >= 0) && (itemPosition < this.mTotalItemsCount)) {
+            return this.mItemsContainerLayout.getChildAt(itemPosition);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//        Log.d(TAG, "onDraw >> mCurrentSlideX" + mCurrentSlideX);
+        this.mLeftDrawRect.set(0, this.mCurrentSlideY, this.mCurrentSlideX, this.mCurrentSlideY + this.mShadow.getHeight());
+        canvas.drawBitmap(this.mShadow, null, this.mLeftDrawRect, null);
+        this.mRightDrawRect.set(this.mCurrentSlideX + this.mSlideIcon.getWidth(), this.mCurrentSlideY, getWidth(), this.mCurrentSlideY + this.mShadow.getHeight());
+        canvas.drawBitmap(this.mShadow, null, this.mRightDrawRect, null);
+        canvas.drawBitmap(this.mSlideIcon, this.mCurrentSlideX, this.mCurrentSlideY, null);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+//        Log.d(TAG, "onLayout");
+        if (!isFirstLayout || mScrollState == ViewPager.SCROLL_STATE_IDLE) {
+            if ((this.mTotalItemsCount > 0) && (getItemView(this.mCurrentItemPosition) != null)) {
+                isFirstLayout = true;
+//                Log.d(TAG, "onLayout this.mTotalItemsCount > 0");
+                View currentItemView = getItemView(this.mCurrentItemPosition);
+                this.mCurrentSlideX = currentItemView.getLeft() + (currentItemView.getWidth() / 2) - (this.mSlideIcon.getWidth() / 2);
+                this.mCurrentSlideY = (b - t - this.mSlideIcon.getHeight());
+//                Log.d(TAG, "onLayout mCurrentSlideX : " + mCurrentSlideX);
+            }
+        }
+    }
+
+    public void setViewPager(ViewPager viewPager) {
+        this.mViewPager = viewPager;
+
+        if (mViewPager.getAdapter() == null) {
+            throw new IllegalStateException("ViewPager does not have adapter instance.");
+        }
+
+        mViewPager.setOnPageChangeListener(mPageListener);
+        notifyDataSetChanged();
+    }
+
+    public void setOnPageChangeListener(ViewPager.OnPageChangeListener changeListener) {
+        this.mDelegatePageListener = changeListener;
+    }
+
+    private void notifyDataSetChanged() {
+        mItemsContainerLayout.removeAllViews();
+        mTotalItemsCount = mViewPager.getAdapter().getCount();
+
+        for (int i = 0; i < mTotalItemsCount; i++) {
+            addItem(i, mViewPager.getAdapter().getPageTitle(i).toString());
+        }
+
+        getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+
+                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                            getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                        } else {
+                            getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        }
+
+                        mCurrentItemPosition = mViewPager.getCurrentItem();
+                        scrollToTab(mCurrentItemPosition, 0);
+                        getItemView(mCurrentItemPosition).setSelected(true);
+                    }
+                });
+    }
+
+    private class PageListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//            Log.d(TAG, "onPageScrolled >> " + "position:" + position +
+//            ",positionOffset:"
+//            + positionOffset + ",positionOffsetPixels:" +
+//            positionOffsetPixels);
+
+            int itemCount = mItemsContainerLayout.getChildCount();
+            if ((itemCount == 0) || (position < 0) || (position >= itemCount)) {
+                return;
+            }
+            mCurrentItemPosition = position;
+
+            View selectedItem = mItemsContainerLayout.getChildAt(position);
+            int extraOffset = (selectedItem != null)
+                    ? (int) (positionOffset * selectedItem.getWidth()) : 0;
+            scrollToTab(position, extraOffset);
+
+//            Log.d(TAG, "extraOffset:" + extraOffset);
+
+            if (mDelegatePageListener != null) {
+                mDelegatePageListener.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+//            Log.d(TAG, "onPageSelected >> " + "position:" + position);
+            setCurrentItemSelected(position);
+            if (mScrollState == ViewPager.SCROLL_STATE_IDLE) {
+//                Log.d(TAG, "onPageSelected SCROLL_STATE_IDLE");
+                scrollToTab(position, 0);
+            }
+            if (mDelegatePageListener != null) {
+                mDelegatePageListener.onPageSelected(position);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+//            Log.d(TAG, "onPageScrollStateChanged >> " + "state:" + state);
+            mScrollState = state;
+            if (mDelegatePageListener != null) {
+                mDelegatePageListener.onPageScrollStateChanged(state);
+            }
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        SavedState savedState = (SavedState) state;
+        super.onRestoreInstanceState(savedState.getSuperState());
+        mCurrentItemPosition = savedState.currentPosition;
+        requestLayout();
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState savedState = new SavedState(superState);
+        savedState.currentPosition = mCurrentItemPosition;
+        return savedState;
+    }
+
+    static class SavedState extends BaseSavedState {
+        int currentPosition;
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        private SavedState(Parcel in) {
+            super(in);
+            currentPosition = in.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeInt(currentPosition);
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+
 }
